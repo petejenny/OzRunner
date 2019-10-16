78d6ee4ef45dfdef0d797150d460a3affd1b0fa3
Peter Forward <petejenny@hotmail.com>
2019-10-16 16:19:16 -0700

    Initial Commit

---
+++ OzRunner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OzRunner
+//
+//  Created by Peter Forward on 10/16/19.
+//  Copyright © 2019 Peter Forward. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
